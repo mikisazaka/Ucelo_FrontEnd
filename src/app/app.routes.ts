7054ee978c1f92fedbec2ed125bd8c2866b49465
Giovana Nunes Constancio <nunes.constancio950@gmail.com>
2025-10-12 15:23:02 -0300

    feat: Criado tela de login.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,34 @@
 import { Routes } from '@angular/router';
 import { PaginaInicialComponent } from './pagina-site/inicial/pagina-inicial/pagina-inicial.component';
 import { LoginComponent } from './pagina-site/inicial/login/login.component';
+import { CadastroComponent } from './pagina-site/inicial/cadastro/cadastro.component';
 
 export const routes: Routes = [
-    { path: 'Ucelo', component: PaginaInicialComponent },
-    { path: 'Login', component: LoginComponent },
-    { path: '**', component: PaginaInicialComponent },
-    { path: '', redirectTo: 'Ucelo', pathMatch: 'full' }
+
+    { 
+        path: 'ucelo', 
+        component: PaginaInicialComponent, 
+    },
+
+    { 
+        path: 'login', 
+        component: LoginComponent 
+    },
+
+    {
+        path: 'cadastro',
+        component: CadastroComponent
+    },
+
+    { 
+        path: '**', 
+        component: PaginaInicialComponent 
+    },
+
+    { 
+        path: '', 
+        redirectTo: 'Ucelo', 
+        pathMatch: 'full' 
+    },
 
 ];
--- src/app/pagina-site/inicial/login/login.component.ts
+++ src/app/pagina-site/inicial/login/login.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy ,Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, RouterOutlet],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    console.log("Login component initialized");
+  }
+
+  goBack(): void {
+    this.router.navigate(['/ucelo']);
+  }
 }
